d91131b6c3bd52b938f9054388cf7232057cbecb
Doyoon Kim <lns8723@gmail.com>
2017-02-03 00:25:01 -0800

    first commit

---
+++ lab5/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using lab5.Models.CityModel;
+
+
+namespace lab5.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext() : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Province> Provinces { get; set; }
+    }
+}
